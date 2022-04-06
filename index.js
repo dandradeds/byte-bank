af316967e7f5200a68b8321e51163784f2101749
Daniel <93725390+dandradeds@users.noreply.github.com>
2022-04-06 14:28:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { Gerencia } from "./Funcionarios/Gerencia.js";
+import { Diretoria } from "./Funcionarios/Diretoria.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretoria ("Daniel", 10000, 55566633390)
+diretor.cadastrarSenha("123456789")
+const gerente = new Gerencia ("Isaias", 5000, 55566633391)
+gerente.cadastrarSenha(123)
 
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const cliente = new Cliente("Roberto", 44499887790, "9876")
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "9876");
 
-console.log(contaSalario);
+console.log(clienteEstaLogado, diretorEstaLogado, gerenteEstaLogado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const cliente1 = new Cliente("Ricardo", 11122233309);
+// // const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
+// // const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+// // const contaSalario = new ContaSalario(cliente1);
+// // contaSalario.depositar(100);
+// // contaSalario.sacar(10);
